7f5329207ef9cd7665f433190b1923138914dbe2
TheMelorenz <gomisha552@gmail.com>
2025-02-10 11:06:02 +0100

    Added timestamp when a task is created

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Добавить дату создания элемента. ✅
+// Добавить дату закрытия таски.
+
+
+
 let tasks = document.getElementById("tasks");
 let createTask = document.getElementById("createTask");
 let getUserTask = document.getElementById("getUserTask");
@@ -6,8 +11,12 @@ createTask.addEventListener("click", function () {
     if(getUserTask.value === '') {
         alert("Please enter your task");
     }else{
+
+
+
         const newElement = document.createElement("div");
         newElement.classList.add("element");
+
 
         const elementTask = document.createElement("div");
         elementTask.classList.add("element_task"); // исправил ошибку
@@ -16,6 +25,9 @@ createTask.addEventListener("click", function () {
         checkbox.type = "checkbox";
 
         const userTask = document.createElement("p");
+
+        const dateCreate = document.createElement("div");
+        dateCreate.classList.add("date_create");
 
         const deleteButton = document.createElement("button");
 
@@ -27,11 +39,17 @@ createTask.addEventListener("click", function () {
         // Делаю HTML-структуру
         elementTask.appendChild(checkbox);
         elementTask.appendChild(userTask);
+        elementTask.appendChild(dateCreate);
         newElement.appendChild(elementTask);
         newElement.appendChild(deleteButton);
 
         // Добавляем новый элемент в контейнер задач
         tasks.appendChild(newElement);
+
+        let now = new Date();
+        dateCreate.style.color = 'rgb(209, 216, 227)';
+        let taskCreated = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        dateCreate.innerHTML = taskCreated;
 
         userTask.innerHTML = getUserTask.value;
         getUserTask.value = "";
@@ -46,11 +64,11 @@ createTask.addEventListener("click", function () {
                 elementTask.style.boxShadow = "none";
                 userTask.style.color = "rgb(209, 216, 227)";
                 userTask.style.textDecoration = "line-through";
+                now = new Date();
             }else{
                 elementTask.style.backgroundColor = "rgb(34, 131, 242)";
                 userTask.style.color = "white";
                 userTask.style.textDecoration = "none";
-
             }
         })
     }
